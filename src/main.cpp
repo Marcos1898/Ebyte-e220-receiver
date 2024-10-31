23ecef338525c6ad6a1820d2be317d46baf33cb0
marcos <100386263@alumnos.uc3m.es>
2024-10-31 11:09:23 +0100

    wifi added

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
  *
  */
 #include "Arduino.h"
+#include <WiFi.h>
 #include "LoRa_E220.h"
 #include "LoRaUtils.h"
 #include "structures.h"
@@ -33,9 +34,27 @@ SoftwareSerial GPSSerial(22, 23);        // RX en GPIO22, TX en GPIO23
 TinyGPSPlus gps;
 const int BOARD_ID = 54321;
 const int pin_led = 4;
+const char *WIFI_SSID = "DIGIFIBRA-ttZU";
+const char *WIFI_PASSWORD = "maraljo1";
 
+void wifi_setup()
+{
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to WiFi");
+  // Muestra la dirección IP
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
 void setup()
 {
+
   pinMode(pin_led, OUTPUT);
   Serial.begin(9600);
   GPSSerial.begin(9600);
@@ -59,6 +78,7 @@ void setup()
     Serial.println("Error al obtener configuración LoRa");
     Serial.println(c.status.getResponseDescription());
   }
+  wifi_setup();
 }
 
 void print_message(Message message, ResponseStructContainer rsc)
@@ -84,36 +104,39 @@ void print_message(Message message, ResponseStructContainer rsc)
   Serial.println();
 }
 
-GPSData read_gps() {
-    // Inicializar un objeto GPSData con valores predeterminados
-    GPSData gps_data = {
-        .lat = -200,
-        .lng = -200,
-        .speed = 0,
-        .date = {0, 0, 0},
-        .time = {0, 0, 0}
-    };
+GPSData read_gps()
+{
+  // Inicializar un objeto GPSData con valores predeterminados
+  GPSData gps_data = {
+      .lat = -200,
+      .lng = -200,
+      .speed = 0,
+      .date = {0, 0, 0},
+      .time = {0, 0, 0}};
 
-    // Leer datos del GPS
-    while (GPSSerial.available() > 0) {
-        gps.encode(GPSSerial.read());
-    }
+  // Leer datos del GPS
+  while (GPSSerial.available() > 0)
+  {
+    gps.encode(GPSSerial.read());
+  }
 
-    // Verifica si la localización es válida
-    if (gps.location.isValid()) {
-        // Obtiene los datos GPS
-        gps_data.lat = gps.location.lat();
-        gps_data.lng = gps.location.lng();
-        gps_data.speed = gps.speed.kmph();
-        gps_data.date = {gps.date.day(), gps.date.month(), gps.date.year()};
-        gps_data.time = {gps.time.hour(), gps.time.minute(), gps.time.second()};
-    } else {
-        Serial.println("Localización no válida.");
-    }
+  // Verifica si la localización es válida
+  if (gps.location.isValid())
+  {
+    // Obtiene los datos GPS
+    gps_data.lat = gps.location.lat();
+    gps_data.lng = gps.location.lng();
+    gps_data.speed = gps.speed.kmph();
+    gps_data.date = {gps.date.day(), gps.date.month(), gps.date.year()};
+    gps_data.time = {gps.time.hour(), gps.time.minute(), gps.time.second()};
+  }
+  else
+  {
+    Serial.println("Localización no válida.");
+  }
 
-    return gps_data;
+  return gps_data;
 }
-
 
 unsigned long previousMillis = 0; // Almacena el último tiempo en que se ejecutó read_gps()
 const long interval = 1000;       // Intervalo de 1 segundo (1000 ms)
@@ -125,7 +148,7 @@ void loop()
   if (currentMillis - previousMillis >= interval)
   {
     previousMillis = currentMillis; // Actualiza el tiempo
-    last_gps = read_gps();                     // Llama a la función cada 1 segundo
+    last_gps = read_gps();          // Llama a la función cada 1 segundo
   }
 
   // Verifica si hay datos disponibles en e220ttl
